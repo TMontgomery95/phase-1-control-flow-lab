9ced718d057479b6e1318fd1a7eff2133f95d1c1
Timothy Montgomery <tim.montgomery95@gmail.com>
2022-08-07 23:19:57 -0400

    commit tree ec8e31b90bada9671e870350acb1250342bf0f3e

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistanceInFeet){
   // Write your code here!
+  if (rideDistanceInFeet <= 400) {
+    return 'This one is on me!';
+  } else if (rideDistanceInFeet >= 400 && rideDistanceInFeet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (rideDistanceInFeet >= 2000 && rideDistanceInFeet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (rideDistanceInFeet > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
 }
